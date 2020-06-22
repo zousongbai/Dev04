e87be203c3dcfd2ee182b6723a7c283aac6197d2
zousongbai <994580004@qq.com>
2020-06-22 23:49:27 +0800

    day622：ORM框架-makemigrations、migrate的用法、以及主键名的更改

--- projects/models.py
+++ projects/models.py
@@ -5,6 +5,13 @@ from django.db import models
 # 2、一个数据模型类对应一个数据表
 # 3、数据模型类，需要继承Model父类或者Model子类，不继承就不是数据模型类，就是普通的类
 # 4、在数据模型类中，添加的类属性（Field对象）来对应数据表中的字段
+# 5、创建完数据库模型类之后，需要迁移才能生成数据表
+# ①生成迁移脚本，放在projects/migrations目录中：python manage.py makemigrations
+# ②执行迁移脚本：python manage.py migrate
+# ③如果不添加选项，那么会将所有子应用进行迁移
+# 6、会自动创建字段名为id的类属性，自增、主键、非空
+# 7、只要某一个字段中primary_key=True，那么django就不会自动创建id字段，会使用自定义的
+
 # （1）步骤一：数据模型类，需要继承Model父类或者Model子类，不继承就不是数据模型类，就是普通的类
 class Projects(models.Model):
     # （2）步骤二：
@@ -12,6 +19,8 @@ class Projects(models.Model):
     # ②需要继承models当中的Model
 
     # （3）步骤三：以类属性的形式添加数据表的字段
+    # 7、只要某一个字段中primary_key=True，那么django就不会自动创建id字段，会使用自定义的
+    id=models.AutoField(primary_key=True)
     # ①项目名称
     name=models.CharField(max_length=200)
     # 备注：
@@ -22,8 +31,9 @@ class Projects(models.Model):
     leader=models.CharField(max_length=50)
 
 
-    # （4）步骤四：往指定的数据表里面自动生成数据表信息
-    # ①makemigrations： 迁移脚本，即：python manage.py makemigrations
-    # ②migrate：创建数据表，即python manage.py migrate
+    # （4）步骤四：创建完数据库模型类之后，需要迁移才能生成数据表
+    # ①生成迁移脚本，放在projects/migrations目录中：python manage.py makemigrations
+    # ②执行迁移脚本：python manage.py migrate
+
 
 
