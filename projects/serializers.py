a51cda333dfc8931ce15a35447b2587ea190410f
zousongbai <994580004@qq.com>
2020-07-04 18:02:18 +0800

    day701:djangorestframework解决痛点3：校验（校验失败的错误信息、字段必填与非必填的设置、修改必填项的报错信息）、总结

--- projects/serializers.py
+++ projects/serializers.py
@@ -7,19 +7,43 @@
 from rest_framework import serializers
 # 继承serializers中的Serializer
 class ProjectsSerializer(serializers.Serializer): # 类名：建议使用：模型类对象+Serializer
+
     """
     可以定义序列化器类，来实现序列化和反序列化操作
     （1）一定要继承serializers.Serializer或者Serializer的子类
     （2）默认情况下，可以定义序列化器字典字段，序列化器字段名要与模型类中字段名相同
-    （3）默认情况下，定义几个序列化器字段，那么就会返回几个数据（到前端）
+    （3）默认情况下，定义几个序列化器字段，那么就会返回几个数据（到前端，序列化输出的过程），前端也必须得传递这几个字段（反序列化过程）
     （4）CharField、BooleanField、IntegerField与模型类中的字段类型一一对应
     （5）required参数默认为None，指定前端必须得传此字段，如果设置为False的话，前端可以不传此字段
     （6）label和help_text->与模型类的verbose_name和help_text一一对应
     （7）alow_null指定前端传递参数时可以传空
-    """
+    （8）CharField字段拥有max_length属性指定该字段不能超过的字节长度
 
-    name=serializers.CharField(max_length=200,label='项目名称',help_text='项目名称')
-    leader = serializers.CharField(max_length=200, label='项目负责人', help_text='项目负责人')
-    tester = serializers.CharField(max_length=200, label='测试人员', help_text='测试人员')
+    """
+    # 修改必填项的报错信息
+
+    # （11）如果某个字段，即没有read_only，也没有write_only，说明此字段既需要反序列化输入，也需要序列化输出
+    name=serializers.CharField(max_length=10,label='项目名称',help_text='项目名称',min_length=2)
+
+
+
+    # （10）如果某个字段指定read_only=True，那么此字段，前端在创建数据时（反序列化过程）可以不用传，但是一定会输出（序列化过程）
+    leader = serializers.CharField(max_length=200, label='项目负责人', help_text='项目负责人',read_only=True)
+    # （12）字段不能同时指定read_only=True，required=True
+    # leader = serializers.CharField(max_length=200, label='项目负责人', help_text='项目负责人', read_only=True,required=True)
+
+
+
+    # （9）如果某个字段指定了write_only = True，那么此字段只能进行反序列化输入，而不会输出（创建数据时必须得传，但是不返回）
+    # tester = serializers.CharField(max_length=200, label='测试人员', help_text='测试人员', write_only=True)
+
+    # （14）可以给字段添加error_messages参数，为字典类型，字典的key为校验的参数名，值为校验失败之后的错误提示
+    tester = serializers.CharField(max_length=200, label='测试人员', help_text='测试人员', write_only=True,
+                                   error_messages={'required':'该字段必传','max_length':'长度不能超过200个字节'})
+    # （13）字段不能同时指定write_only=True，read_only=True
+    # tester = serializers.CharField(max_length=200, label='测试人员', help_text='测试人员', write_only=True,read_only=True)
+
+
     # label：人性化的说明
     # help_text：API接口的中文提示
+    # models中只能指定最大长度，序列化器中既可以指定最大长度，也可以指定最小长度
--- projects/views.py
+++ projects/views.py
@@ -5,6 +5,12 @@ from django.views import View
 from projects.models import Projects
 from .serializers import ProjectsSerializer
 
+ret = {
+    "msg": "",
+    "code": 0
+}
+
+
 # 使用两个类视图：是因为有的接口需要传递id，有的接口不需要传递id，把传id的放在一个类似图中，不传id的放在另外一个类视图中
 class ProjectsView(View):
     """
@@ -41,7 +47,7 @@ class ProjectsView(View):
         python_dict['data'] = python_data
 
         # 创建序列化器对象
-        serializer_obj=ProjectsSerializer(instance=projects_object,many=True)
+        serializer_obj = ProjectsSerializer(instance=projects_object, many=True)
         # 备注：
         # ①可以使用序列化器类来进行序列化输出
         # 1）instance：可以传模型类对象，
@@ -72,8 +78,6 @@ class ProjectsView(View):
             }
             return JsonResponse(result, status=400)
 
-
-
         # （2）步骤二：校验传递的数据是否正确（非常复杂）
         # if ('name' not in python_data) or ('leader' not in python_data):
         #     #  判断name或leader不在python_data中，则报错误
@@ -83,25 +87,30 @@ class ProjectsView(View):
         #     }
         #     return JsonResponse(result, status=400)
 
-        ret = {
-            "msg": "",
-            "code": 0
-        }
-
         # ①步骤一：创建序列化器类对象
         # 1）把前端传递的json格式参数转化为字典之后，传递给data参数
         # 2）序列化器对象.is_valid()方法，开始进行校验，如果不调用此方法，那么不会进行校验
         # 3）调用序列化器对象.is_valid()方法，如果校验成功，返回True，否则返回False
         # 4）必须调用is_valid()方法之后，才能使用.errors属性去获取报错信息，相当于一个字典
-        serializer_obj1=ProjectsSerializer(data=python_data)
-        if not serializer_obj1.is_valid():
-            # 如果校验失败，将错误信息传给JsonResponse
+        # 5）必须调用is_valid()方法之后，才能使用.validated_data属性去获取校验通过信息，相当于一个字典
+        serializer_obj1 = ProjectsSerializer(data=python_data)
+        try:
+            # raise_exception=True：当校验失败，会报异常
+            serializer_obj1.is_valid(raise_exception=True)
+        except Exception as e:
             ret['msg'] = '参数有误'
             ret.update(serializer_obj1.errors)
-            return JsonResponse(ret, safe=False,status=400)
+            return JsonResponse(ret, status=400)
+
+        # if not serializer_obj1.is_valid():
+        #     # 如果校验失败，将错误信息传给JsonResponse
+        #     ret['msg'] = '参数有误'
+        #     ret.update(serializer_obj1.errors)
+        #     return JsonResponse(ret, safe=False,status=400)
 
         # （3）步骤三：创建模型类对象
-        obj = Projects.objects.create(**python_data)
+
+        obj = Projects.objects.create(**serializer_obj1.validated_data)
 
         # （4）步骤四：向前端返回json格式的数据
         # python_dict = {
@@ -114,9 +123,12 @@ class ProjectsView(View):
         #     'msg': '创建成功'
         # }
 
-        serializer_obj = ProjectsSerializer(instance=obj)
+        # serializer_obj = ProjectsSerializer(instance=obj)
         # 备注：因为职业应该对象，不是查询集，所以不需要加many=True
-        return JsonResponse(serializer_obj.data, status=201)
+
+        ret['msg'] = '成功'
+        ret.update(serializer_obj1.validated_data)
+        return JsonResponse(ret, status=201)
 
 
 class ProjectDetailView(View):
@@ -136,11 +148,12 @@ class ProjectDetailView(View):
    ③request data：请求的数据json格式
    ④response data：返回的数据json格式 备注：URL相同的说明需要在同一个类视图下面创建不同的实例方法
     """
-    def get(self,request,pk):
+
+    def get(self, request, pk):
         """获取项目详情"""
         # （1）步骤一：校验参数，校验参数pk是否存在
         try:
-            obj=Projects.objects.get(id=pk) # get：结果没有或返回多条结果都会报错
+            obj = Projects.objects.get(id=pk)  # get：结果没有或返回多条结果都会报错
         except Exception as e:
             result = {
                 'msg': '参数有误',
@@ -158,18 +171,18 @@ class ProjectDetailView(View):
         # }
 
         # ①进行序列化输出，需要创建序列化器类对象
-        serializer_obj=ProjectsSerializer(instance=obj) # instance：可以接收模型类对象，也可以接收查询集对象。返回单个数据是，此处是模型类对象
+        serializer_obj = ProjectsSerializer(instance=obj)  # instance：可以接收模型类对象，也可以接收查询集对象。返回单个数据是，此处是模型类对象
         # ②获取数据：使用序列化器对象的data属性
-        python_dict=serializer_obj.data
+        python_dict = serializer_obj.data
 
         # （3）步骤三：向前端返回json格式的数据
         return JsonResponse(python_dict)
 
-    def put(self,request,pk):
+    def put(self, request, pk):
         """更新项目"""
         # （1）步骤一：校验pk值否存在，并获取待更新的模型类对象
         try:
-            obj=Projects.objects.get(id=pk) # get：结果没有或返回多条结果都会报错
+            obj = Projects.objects.get(id=pk)  # get：结果没有或返回多条结果都会报错
         except Exception as e:
             result = {
                 'msg': '参数有误',
@@ -199,8 +212,8 @@ class ProjectDetailView(View):
 
         # （3）步骤三：更新操作
         # 如果前端传空就更新，如果不为空则更新
-        obj.name=python_data.get('name') or obj.name
-        obj.leader=python_data.get('leader') or obj.leader
+        obj.name = python_data.get('name') or obj.name
+        obj.leader = python_data.get('leader') or obj.leader
         obj.tester = python_data.get('tester') or obj.tester
         obj.programmer = python_data.get('programmer') or obj.programmer
         obj.desc = python_data.get('desc') or obj.desc
@@ -221,11 +234,11 @@ class ProjectDetailView(View):
         #     'msg': '更新成功'
         # }
 
-        serializer_obj=ProjectsSerializer(instance=obj) # 序列化输出时，使用这行代码
+        serializer_obj = ProjectsSerializer(instance=obj)  # 序列化输出时，使用这行代码
 
         return JsonResponse(serializer_obj.data, status=201)
 
-    def delete(self,request,pk):
+    def delete(self, request, pk):
         """删除项目"""
         # （1）步骤一：校验pk值否存在，并获取待删除的模型类对象
         try:
@@ -241,12 +254,11 @@ class ProjectDetailView(View):
         obj.delete()
 
         # 返回
-        python_data={
-            'msg':'删除成功',
-            'code':1
+        python_data = {
+            'msg': '删除成功',
+            'code': 1
         }
-        return JsonResponse(python_data,status=200)
-
+        return JsonResponse(python_data, status=200)
 
 # summary：
 # （1）上述5个接口的实现步骤
