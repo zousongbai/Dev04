46eb97e7e26c7247cfed5013999bcdf6b53fc4c5
zousongbai <994580004@qq.com>
2020-07-21 22:18:20 +0800

    day720：添加全局路由Dev04/urls.py配置rest_framework的子路由

--- Dev04/settings.py
+++ Dev04/settings.py
@@ -172,7 +172,23 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 3,
 
     # 指定用于支持coreapi的Schema
-    'DEFAULT_SCHEMA_CLASS':'rest_framework.schemas.coreapi.AutoSchema'
+    'DEFAULT_SCHEMA_CLASS':'rest_framework.schemas.coreapi.AutoSchema',
+
+    # DEFAULT_AUTHENTICATION_CLASSES：指定默认的认证类（认证方式）
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        # 会话认证
+        'rest_framework.authentication.SessionAuthentication',
+        # 基本认证（用户名和密码认证），在测试或开发环境使用，生成环境不用
+        'rest_framework.authentication.BasicAuthentication'
+    ],
+
+    # DEFAULT_PERMISSION_CLASSES：指定认证之后，能获取到的权限
+    'DEFAULT_PERMISSION_CLASSES': [
+        # ①AllowAny：不需要登录就有任意权限
+        # 'rest_framework.permissions.AllowAny',
+        # ②IsAuthenticated：只要登录之后，就具体任务权限
+        'rest_framework.permissions.IsAuthenticated',
+    ],
 }
 
 
--- Dev04/urls.py
+++ Dev04/urls.py
@@ -17,21 +17,21 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from rest_framework.documentation import include_docs_urls
 
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
+# from drf_yasg.views import get_schema_view
+# from drf_yasg import openapi
 
-schema_view = get_schema_view(
-    openapi.Info(
-        title="Lemon API接口文档平台",    # 必传
-        default_version='v1',   # 必传
-        description="这是一个美轮美奂的接口文档",
-        terms_of_service="http://api.keyou.site",
-        contact=openapi.Contact(email="keyou100@qq.com"),
-        license=openapi.License(name="BSD License"),
-    ),
-    public=True,
-    # permission_classes=(permissions.AllowAny,),   # 权限类
-)
+# schema_view = get_schema_view(
+#     openapi.Info(
+#         title="Lemon API接口文档平台",    # 必传
+#         default_version='v1',   # 必传
+#         description="这是一个美轮美奂的接口文档",
+#         terms_of_service="http://api.keyou.site",
+#         contact=openapi.Contact(email="keyou100@qq.com"),
+#         license=openapi.License(name="BSD License"),
+#     ),
+#     public=True,
+#     # permission_classes=(permissions.AllowAny,),   # 权限类
+# )
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -39,13 +39,14 @@ urlpatterns = [
     # 添加接口文档平台的路由条目
     path('docs/',include_docs_urls(title='测试平台接口文档',description='xxx描述')),
 
-    # 使用drf-yasg
-    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    # # 使用drf-yasg
+    # re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    # path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    # path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
     path('',include('projects.urls')), # 使用include将子路由的路径传进去
+
+    #
+    path('api/',include('rest_framework.urls')),
 ]
-# 分析：
-# path('admin/', admin.site.urls)：去匹配，没有匹配，则进入下一行进行匹配
-# path('',include('projects.urls'))：匹配为空，则直接去子路由进行匹配
+
